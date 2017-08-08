1f2d016c661ef20696de7d82ee9752b394052758
Dolma <dolma@Owners-MacBook-Pro.local>
2017-08-08 14:09:51 -0400

    first exercise

--- exercise1.rb
+++ exercise1.rb
@@ -3,4 +3,12 @@ puts 2 + 3
 puts 2
 puts 3
  puts 2 + 3
- 
+
+ puts " Hello \t \t world"
+
+puts " Hello \b\b\b\b\b Goodbye world"
+
+puts " Hello \r Start over world"
+puts " hello \r world"
+
+puts " 1. Hello \n2. world"
