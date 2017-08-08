9ddd76c444dbc35b4ec036b8a7e395621bb93724
Dolma <dolma@Owners-MacBook-Pro.local>
2017-08-08 16:00:08 -0400

    exercise 4 question 4

---
+++ exercise4_4.rb
@@ -0,0 +1,10 @@
+puts "Enter your name"
+user_name = gets.chomp
+
+if (user_name.length > 10)
+  puts " Hi! #{user_name}"
+elsif (user_name.length < 10)
+  puts "Hello! #{user_name}"
+else
+  puts "Hey! #{user_name}"
+end
