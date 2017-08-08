2e826083f6c694817f570a962b3dc0378c688b5a
Dolma <dolma@Owners-MacBook-Pro.local>
2017-08-08 16:13:29 -0400

    exercise 4 question 5

--- exercise3.rb
+++ exercise3.rb
@@ -1,7 +1,7 @@
 puts "What is your name?"
-user_name = gets.chomp
-puts "Hello, #{user_name}"
-
+user_name = gets.chomp     # gets.chomp will not include extra new line after input value
+puts "Hello, #{user_name}"   # whereas # gets. will take extra new line afer input value in result
+                              # always use gets.chomp for string
 puts "How old are you ?"
 user_age = gets.chomp
 age = user_age.to_i
---
+++ exercise4_5.rb
@@ -0,0 +1,10 @@
+secret_number = 7
+puts "Guess my secret number"
+user_number = gets.to_i
+if (secret_number == user_number)
+  puts "You Win!"
+elsif (secret_number - user_number == 1) || (secret_number - user_number == -1)
+  puts "Your are very close!"
+else
+  puts "Try Again!"
+end
