bc0cee9c6c55ec9e2e6559ac0b677df13b42fcc2
Dolma <dolma@Owners-MacBook-Pro.local>
2017-08-08 17:11:26 -0400

    exercise 5 question related to while loop

---
+++ exercise5.rb
@@ -0,0 +1,15 @@
+
+
+count = 0
+while true do
+  puts "Would like to walk or run?"
+  user_choice = gets.chomp
+  
+  if (user_choice == "walk")
+    count += 1
+  puts "Distance from home is #{count}km"
+elsif
+    count += 5
+    puts "Distance from home is #{count}"
+end
+end
