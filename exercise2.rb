2ef02d202c89b1254828acdc80254e848cbf0141
Dolma <dolma@Owners-MacBook-Pro.local>
2017-08-08 14:33:06 -0400

    exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+# Calculating good tip for 55 dollar meal
+puts " Tip for the 55 dollar meal is #{0.13*55}"
+
+# Adding string and integer
+puts "canada" + "#{150.to_s}"
+
+# applying string interpolation
+puts " Multiple of 45628 and 7839 is #{45628*7839}"
+
+# Boolean logical question
+puts (10 < 20 && 30 < 20) || ! (10 == 11)
