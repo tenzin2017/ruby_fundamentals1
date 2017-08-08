74d1646d051887e3211a84ac32d89c833e0546bf
Dolma <dolma@Owners-MacBook-Pro.local>
2017-08-08 15:53:26 -0400

    exercise 4 question 2

--- exercise3.rb
+++ exercise3.rb
@@ -6,10 +6,3 @@ puts "How old are you ?"
 user_age = gets.chomp
 age = user_age.to_i
 puts " #{user_name} was born in #{2017 - age}"
-
-user_no = gets.to_i
-if (user_no >= 100)
-  puts "That's a very big number !"
-else
-  puts " Very good choice !"
-end
---
+++ exercise4_3.rb
@@ -0,0 +1,11 @@
+
+
+puts "Enter your name"
+user_name = gets.chomp
+my_name = "lobsang"
+
+if (my_name == user_name)
+  puts " We have the same name !"
+else
+  puts " Hello, #{user_name}"
+end
