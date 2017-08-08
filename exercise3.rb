24a02efb3208451c7ec6fc82ec6788b9589de287
Dolma <dolma@Owners-MacBook-Pro.local>
2017-08-08 15:08:55 -0400

    finding the year of birth

--- exercise3.rb
+++ exercise3.rb
@@ -1,6 +1,8 @@
-puts "What is your name ?"
-user_name = gets
-puts "Hello, #{user_name}"
-
+puts "What is your name?"
 user_name = gets.chomp
 puts "Hello, #{user_name}"
+
+puts "How old are you ?"
+user_age = gets.chomp
+age = user_age.to_i
+puts " #{user_name} was born in #{2017 - age}"
